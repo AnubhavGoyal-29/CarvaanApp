4d1078100a49822f987661af5099a3aaf6f298e6
ReCreateIndia <wewillrecreateindia@gmail.com>
2020-10-21 19:01:34 +0530

    bla

--- MyApplication/app/src/main/java/startup/carvaan/myapplication/ui/start/SplashScreen.java
+++ MyApplication/app/src/main/java/startup/carvaan/myapplication/ui/start/SplashScreen.java
@@ -1,14 +1,57 @@
-package startup.carvaan.myapplication;
+package startup.carvaan.myapplication.ui.start;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import startup.carvaan.myapplication.R;
+import startup.carvaan.myapplication.ui.login.LoginActivity;
+import startup.carvaan.myapplication.ui.mainActivity.MainActivity;
 
 public class SplashScreen extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    ImageView app_logo;
+    TextView app_motto, app_name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        app_logo = findViewById(R.id.applogo);
+        app_motto = findViewById(R.id.appmotto);
+        app_name = findViewById(R.id.appname);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                gotonextpage();
+            }
+        }, 1000);
+
+    }
+
+    private void gotonextpage() {
+        startActivity(new Intent(SplashScreen.this,MainActivity.class));
+//        if (firebaseUser != null) {
+//            Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+//            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            startActivity(mainIntent);
+//        } else {
+//            Intent mainIntent = new Intent(SplashScreen.this, LoginActivity.class);
+//            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            startActivity(mainIntent);
+//        }
     }
 }
