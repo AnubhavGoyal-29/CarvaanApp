1297528065d0afdf3e0713eb7883902ee552e45e
ReCreateIndia <wewillrecreateindia@gmail.com>
2020-10-21 11:48:41 +0530

    newer

---
+++ MyApplication/app/src/main/java/startup/carvaan/myapplication/ui/about/AboutShare.java
@@ -0,0 +1,16 @@
+package startup.carvaan.myapplication.ui.about;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import startup.carvaan.myapplication.R;
+
+public class AboutShare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_share);
+    }
+}
