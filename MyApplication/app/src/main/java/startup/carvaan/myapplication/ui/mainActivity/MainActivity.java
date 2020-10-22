d3d05b87989598a28c4d79bfb5400fa19a9e1d21
ReCreateIndia <wewillrecreateindia@gmail.com>
2020-10-22 16:34:18 +0530

    values

--- MyApplication/app/src/main/java/startup/carvaan/myapplication/ui/mainActivity/MainActivity.java
+++ MyApplication/app/src/main/java/startup/carvaan/myapplication/ui/mainActivity/MainActivity.java
@@ -2,30 +2,43 @@ package startup.carvaan.myapplication.ui.mainActivity;
 
 
 import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.sdsmdg.harjot.vectormaster.VectorMasterView;
 import com.sdsmdg.harjot.vectormaster.models.PathModel;
 
 import androidx.annotation.NonNull;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import startup.carvaan.myapplication.R;
+import startup.carvaan.myapplication.ui.login.LoginActivity;
 
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -34,25 +47,19 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     private VectorMasterView heartVector1;
     private VectorMasterView heartVector2;
     private float mYVal;
+    private ActionBarDrawerToggle toggle;
     private RelativeLayout mlinId;
     PathModel outline;
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setElevation(0);
+        getSupportActionBar().setTitle("Karvaan");
+        getSupportActionBar().setDisplayOptions(android.app.ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
         getSupportActionBar().setTitle("Carvaan");
-
-
         mView= findViewById(R.id.aboutsharebottomnavview);
-
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.myshares, R.id.allshares, R.id.earncredits)
                 .build();
@@ -68,8 +75,51 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         mView.inflateMenu(R.menu.bottom_nav_menu);
         mView.setSelectedItemId(R.id.allshares);
         mView.setOnNavigationItemSelectedListener(MainActivity.this);
+        // drawer
+        NavigationView navigationView =findViewById(R.id.n1);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.logout:
+                        Toast.makeText(MainActivity.this,"yo",Toast.LENGTH_LONG).show();
+                        FirebaseAuth firebaseAuth;
+                        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+                        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(MainActivity.this,googleSignInOptions);
+                        if(googleSignInClient!=null) {
+                            googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
 
 
+                                }
+                            });
+                        }
+                        gotoLoginActivity();
+                        break;
+                }
+
+                return false;
+            }
+        });
+        DrawerLayout drawerLayout = findViewById(R.id.drawerlayout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item))
+            return true;
+        return false;
+    }
+    private void gotoLoginActivity() {
+
+        Intent logIntent = new Intent(MainActivity.this, LoginActivity.class);
+        logIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(logIntent);
     }
 
     @Override
