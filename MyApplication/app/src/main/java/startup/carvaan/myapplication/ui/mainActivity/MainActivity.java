26b089c135bdc563e02638fd46fd33e12d29563a
ReCreateIndia <wewillrecreateindia@gmail.com>
2020-10-22 17:08:05 +0530

    clever

--- MyApplication/app/src/main/java/startup/carvaan/myapplication/ui/mainActivity/MainActivity.java
+++ MyApplication/app/src/main/java/startup/carvaan/myapplication/ui/mainActivity/MainActivity.java
@@ -2,12 +2,23 @@ package startup.carvaan.myapplication.ui.mainActivity;
 
 
 import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -17,71 +28,73 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.sdsmdg.harjot.vectormaster.VectorMasterView;
 import com.sdsmdg.harjot.vectormaster.models.PathModel;
 
-import androidx.annotation.NonNull;
-
-import android.widget.Toast;
-
-
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
+import javax.annotation.Nullable;
 import startup.carvaan.myapplication.R;
+import startup.carvaan.myapplication.ui.allshares.allshares;
+import startup.carvaan.myapplication.ui.earncredits.earncredits;
 import startup.carvaan.myapplication.ui.login.LoginActivity;
+import startup.carvaan.myapplication.ui.mainActivity.CurvedNavigationBottomView;
+import startup.carvaan.myapplication.ui.myshares.myshares;
 
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "carvaan";
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    DrawerLayout drawerLayout;
+    FirebaseFirestore ff;
+    GestureDetector gestureDetector;
+    private ActionBarDrawerToggle toggle;
     private CurvedNavigationBottomView mView;
     private VectorMasterView heartVector;
     private VectorMasterView heartVector1;
     private VectorMasterView heartVector2;
     private float mYVal;
-    private ActionBarDrawerToggle toggle;
     private RelativeLayout mlinId;
     PathModel outline;
+    private ActionBar actionBar;
+    TextView coins;
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getSupportActionBar().setElevation(0);
         getSupportActionBar().setTitle("Karvaan");
         getSupportActionBar().setDisplayOptions(android.app.ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.abs_layout);
-        getSupportActionBar().setTitle("Carvaan");
-
-
-        mView= findViewById(R.id.bottom_nav_view);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.myshares, R.id.allshares, R.id.earncredits)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        NavigationUI.setupWithNavController(mView, navController);
-
+        View view= getSupportActionBar().getCustomView();
+        coins=view.findViewById(R.id.coins);
+        ff=FirebaseFirestore.getInstance();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new allshares()).commit();
+        mView= findViewById(R.id.aboutsharebottomnavview);
         heartVector = findViewById(R.id.fab);
         heartVector1 = findViewById(R.id.fab1);
         heartVector2 = findViewById(R.id.fab2);
+
         mlinId = findViewById(R.id.lin_id);
         mView.inflateMenu(R.menu.bottom_nav_menu);
         mView.setSelectedItemId(R.id.allshares);
+
+
         mView.setOnNavigationItemSelectedListener(MainActivity.this);
-        // drawer
+
         NavigationView navigationView =findViewById(R.id.n1);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.logout:
-                        Toast.makeText(MainActivity.this,"yo",Toast.LENGTH_LONG).show();
-                        FirebaseAuth firebaseAuth;
+                        firebaseAuth.signOut();
                         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
                         GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(MainActivity.this,googleSignInOptions);
                         if(googleSignInClient!=null) {
@@ -100,21 +113,65 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 return false;
             }
         });
-        DrawerLayout drawerLayout = findViewById(R.id.drawerlayout);
+        drawerLayout = findViewById(R.id.drawerlayout);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser!=null) {
+            ff.collection("Users").document(firebaseUser.getUid()).collection("Credits")
+                    .document("Credits").addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                    DocumentSnapshot snapshot = documentSnapshot;
+
+                }
+            });
+        }
+
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.coin_menu,menu);
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (toggle.onOptionsItemSelected(item))
-            return true;
-        return false;
-    }
-    private void gotoLoginActivity() {
+        if (toggle.onOptionsItemSelected(item)) {
 
+            return true;
+        }
+        switch (item.getItemId()) {
+            case R.id.logout:
+                firebaseAuth.signOut();
+                GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+                GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(MainActivity.this,googleSignInOptions);
+                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+
+                    }
+                });
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private void gotoLoginActivity() {
+        firebaseAuth.signOut();
+        firebaseUser=null;
         Intent logIntent = new Intent(MainActivity.this, LoginActivity.class);
         logIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(logIntent);
@@ -122,8 +179,11 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
         switch (item.getItemId()) {
             case R.id.myshares:
+                selectedFragment = new myshares();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
 
 
                 tet(6);
@@ -136,16 +196,21 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 selectAnimation(heartVector);
                 break;
             case R.id.allshares:
-
+                selectedFragment = new allshares();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
 
                 tet(2);
                 mlinId.setX(mView.mFirstCurveControlPoint1.x );
                 heartVector.setVisibility(View.GONE);
                 heartVector1.setVisibility(View.VISIBLE);
                 heartVector2.setVisibility(View.GONE);
+
+
                 selectAnimation(heartVector1);
                 break;
             case R.id.earncredits:
+                selectedFragment = new earncredits();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
 
                 tet();
                 mlinId.setX(mView.mFirstCurveControlPoint1.x ) ;
@@ -157,8 +222,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 selectAnimation(heartVector2);
                 break;
         }
-
-
 
         return true;
     }
@@ -230,5 +293,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         mView.mSecondCurveControlPoint1.set(mView.mSecondCurveStartPoint.x + (mView.CURVE_CIRCLE_RADIUS * 2) - mView.CURVE_CIRCLE_RADIUS, mView.mSecondCurveStartPoint.y);
         mView.mSecondCurveControlPoint2.set(mView.mSecondCurveEndPoint.x - (mView.CURVE_CIRCLE_RADIUS + (mView.CURVE_CIRCLE_RADIUS / 4)), mView.mSecondCurveEndPoint.y);
     }
+
 
 }
