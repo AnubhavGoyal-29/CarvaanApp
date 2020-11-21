151831e3bd64f074a004d78ea3b11e3433c593c0
AnubhavWorld <18ucs066@lnmiit.ac.in>
2020-11-21 11:36:04 +0530

    haha

---
+++ MyApplication/app/src/main/java/startup/carvaan/myapplication/ChartActivity.java
@@ -0,0 +1,78 @@
+package startup.carvaan.myapplication;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+public class ChartActivity extends AppCompatActivity implements OnChartGestureListener, OnChartValueSelectedListener {
+
+    private LineChart mchart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart);
+        mchart=findViewById(R.id.lineChart);
+        mchart.setOnChartGestureListener(ChartActivity.this);
+        mchart.setOnChartValueSelectedListener(ChartActivity.this);
+        mchart.setDragEnabled(true);
+        mchart.setScaleEnabled(true);
+    }
+
+    @Override
+    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
+}
